4a82f7dc26bd463205bd89bc5f876d7ba8839852
mosfeqanik <mosfeqanik01@gmail.com>
2024-12-23 17:55:20 +0600

    Initial Commit

---
+++ hike_app/hike_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  hike_appApp.swift
+//  hike_app
+//
+//  Created by Togu Mogu on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct hike_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
