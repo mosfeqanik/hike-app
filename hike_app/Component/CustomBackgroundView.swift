1546d1d6c1c6096e3f4e5c0762ba57c5bee89553
mosfeqanik <mosfeqanik01@gmail.com>
2024-12-30 18:11:11 +0600

    color and card design added

---
+++ hike_app/Component/CustomBackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomBackgroundView.swift
+//  hike_app
+//
+//  Created by Togu Mogu on 30/12/24.
+//
+
+import SwiftUI
+
+struct CustomBackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomBackgroundView()
+}
---
+++ hike_app/Utility/ColorExtension.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExtension.swift
+//  hike_app
+//
+//  Created by Togu Mogu on 30/12/24.
+//
+
+import Foundation
