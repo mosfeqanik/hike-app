8eb51c6979c3407b172715374a0a7ee06d33bdf9
mosfeqanik <mosfeqanik01@gmail.com>
2025-01-01 16:43:33 +0600

    color extention added

--- hike_app/Utility/ColorExtension.swift
+++ hike_app/Utility/ColorExtension.swift
@@ -8,8 +8,8 @@
 import Foundation
 import SwiftUI
 extension Color{
-    static let customGreenLight = Color("ColorGreenLight")
-    static let customGreenMedium = Color("ColorGreenMedium")
+    static let customGrayLight = Color("ColorGrayLight")
+    static let customGrayMedium = Color("ColorGrayMedium")
     static let customGreenDark = Color("ColorGreenDark")
     static let customGreenLight = Color("ColorGreenLight")
     static let customGreenMedium = Color("ColorGreenMedium")
