c204ae4390d96d8e4abdc977a3a5729067df68d2
mosfeqanik <mosfeqanik01@gmail.com>
2025-01-01 16:42:16 +0600

    color extention added

--- hike_app/Utility/ColorExtension.swift
+++ hike_app/Utility/ColorExtension.swift
@@ -11,6 +11,8 @@ extension Color{
     static let customGreenLight = Color("ColorGreenLight")
     static let customGreenMedium = Color("ColorGreenMedium")
     static let customGreenDark = Color("ColorGreenDark")
-    static let customGreenLight = Color("ColorGrayLight")
-    static let customGreenLight = Color("ColorGrayMedium")
+    static let customGreenLight = Color("ColorGreenLight")
+    static let customGreenMedium = Color("ColorGreenMedium")
+    static let customIndigoMedium = Color("ColorIndigoMedium")
+    static let customSalmonLight = Color("ColorSalmonLight")
 }
