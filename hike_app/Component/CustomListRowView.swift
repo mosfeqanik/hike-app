f20759c36f9158e693c75df55e2b30c01f2bc77a
mosfeqanik <mosfeqanik01@gmail.com>
2025-08-01 19:24:13 +0600

    LInk usage added row linklabel and row link destination added

--- hike_app/Component/CustomListRowView.swift
+++ hike_app/Component/CustomListRowView.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct CustomListRowView: View {
+    // MARK; - PROPERTIES
+    
+    @State var rowLabel: String
+    @State var rowIcon: String
+    @State var rowContent: String? = nil
+    @State var rowTintColor: Color
+    @State var rowLinkLabel: String? = nil
+    @State var rowLinkDestination: String? = nil
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LabeledContent{
+            // Content
+            if rowContent != nil
+            {
+                Text(rowContent!)
+                    .foregroundColor(.primary)
+                    .fontWeight(.heavy)
+            }else if (rowLinkLabel != nil && rowLinkDestination != nil){
+                Link(rowLinkLabel!,
+                     destination: URL(string: rowLinkDestination!)!)
+                .foregroundColor(.pink)
+                .fontWeight(.heavy)
+            }else{
+                EmptyView()
+            }
+        }label: {
+            // Label
+            HStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8)
+                        .frame(width:30 ,height: 30)
+                        .foregroundColor(rowTintColor)
+                    Image(systemName: rowIcon)
+                        .foregroundColor(.white)
+                        .fontWeight(.semibold)
+                }
+                Text(rowLabel)
+            }
+        }
     }
 }
 
 #Preview {
-    CustomListRowView()
+    List {
+        CustomListRowView(
+            rowLabel: "Designer",
+            rowIcon: "paintpalette",
+            rowContent: nil,
+            rowTintColor: .pink,
+            rowLinkLabel: "Portfolio",
+            rowLinkDestination: "https://mosfeqanik.github.io"
+        )
+    }
 }
