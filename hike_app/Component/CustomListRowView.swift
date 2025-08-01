24915cb97ffa4b51addb825c085a30e17b2e8dba
mosfeqanik <mosfeqanik01@gmail.com>
2025-08-01 18:14:49 +0600

    added in Settings views
    Basic Labeled Content & Advanced Labeled Content

---
+++ hike_app/Component/CustomListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomListRowView.swift
+//  hike_app
+//
+//  Created by Md Mosfeq Anik on 1/8/25.
+//
+
+import SwiftUI
+
+struct CustomListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomListRowView()
+}
